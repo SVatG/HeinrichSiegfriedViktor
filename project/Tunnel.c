32bdf8253e89a304232c9641d18506b0c750520e
Dag Ågren <paracelsus@gmail.com>
2011-04-23 21:45:31 +0300

    Tunnel v2
    --HG--
    branch : WAHa

--- project/Tunnel.c
+++ project/Tunnel.c
@@ -4,20 +4,20 @@
 #include "DS3D/Utils.h"
 
 #define NumSections 16
-#define NumParts 4
+#define NumParts 7
 #define PartSize 1024
 
 typedef struct TunnelSection
 {
-	int part;
+	int part,hue;
 	int32_t x,y;
 } TunnelSection;
 
 TunnelSection tunnel[NumSections];
-int lastendsection;
-int32_t x,y;
+int lastendsection,currhue;
+int32_t x,y,sx,sy,sdx,sdy,sa;
 
-uint32_t parts[NumParts-1][PartSize];
+uint32_t parts[NumParts][PartSize];
 
 static void MakeParts()
 {
@@ -147,6 +147,136 @@ static void MakeParts()
 
 	if(DSFinishList()>PartSize) for(;;);
 
+
+
+	const float b=0.7;
+	DSStartList(parts[3],PartSize);
+
+	DSListBegin(DS_QUADS);
+
+	DSListNormal3f(0,-1,0);
+	DSListVertex3f(3,b,0);
+	DSListVertex3f(3,b,-1);
+	DSListVertex3f(-3,b,-1);
+	DSListVertex3f(-3,b,0);
+
+	DSListNormal3f(0,1,0);
+	DSListVertex3f(3,-b,0);
+	DSListVertex3f(3,-b,-1);
+	DSListVertex3f(-3,-b,-1);
+	DSListVertex3f(-3,-b,0);
+
+	DSListNormal3f(0,0,1);
+	DSListVertex3f(3,3,0);
+	DSListVertex3f(3,b,0);
+	DSListVertex3f(-3,b,0);
+	DSListVertex3f(-3,3,0);
+
+	DSListVertex3f(3,-3,0);
+	DSListVertex3f(3,-b,0);
+	DSListVertex3f(-3,-b,0);
+	DSListVertex3f(-3,-3,0);
+
+	DSListEnd();
+
+	if(DSFinishList()>PartSize) for(;;);
+
+
+
+	DSStartList(parts[4],PartSize);
+
+	DSListBegin(DS_QUADS);
+
+	DSListNormal3f(0,-1,0);
+	DSListVertex3f(b,3,0);
+	DSListVertex3f(b,3,-1);
+	DSListVertex3f(b,-3,-1);
+	DSListVertex3f(b,-3,0);
+
+	DSListNormal3f(0,1,0);
+	DSListVertex3f(-b,3,0);
+	DSListVertex3f(-b,3,-1);
+	DSListVertex3f(-b,-3,-1);
+	DSListVertex3f(-b,-3,0);
+
+	DSListNormal3f(0,0,1);
+	DSListVertex3f(3,3,0);
+	DSListVertex3f(b,3,0);
+	DSListVertex3f(b,-3,0);
+	DSListVertex3f(3,-3,0);
+
+	DSListVertex3f(-3,3,0);
+	DSListVertex3f(-b,3,0);
+	DSListVertex3f(-b,-3,0);
+	DSListVertex3f(-3,-3,0);
+
+	DSListEnd();
+
+	if(DSFinishList()>PartSize) for(;;);
+
+
+
+	const int32_t r=F(0.7);
+	DSStartList(parts[5],PartSize);
+
+	DSListBegin(DS_QUAD_STRIP);
+	for(int i=0;i<=16;i++)
+	{
+		int sin_a=isin(F(i)/16);
+		int cos_a=icos(F(i)/16);
+		DSListNormal3n10(DSf32ton10(-cos_a),DSf32ton10(-sin_a),0);
+		DSListVertex3v16(imul(cos_a,r),imul(sin_a,r),F(0));
+		DSListVertex3v16(imul(cos_a,r),imul(sin_a,r),F(-1));
+	}
+	DSListEnd();
+
+	DSListBegin(DS_QUAD_STRIP);
+	DSListNormal3f(0,0,1);
+	for(int i=0;i<=16;i++)
+	{
+		int sin_a=isin(F(i)/16);
+		int cos_a=icos(F(i)/16);
+		DSListVertex3v16(imul(cos_a,r),imul(sin_a,r),F(0));
+		DSListVertex3v16(imul(cos_a,F(3.9)),imul(sin_a,F(3.9)),F(0));
+	}
+	DSListEnd();
+
+	if(DSFinishList()>PartSize) for(;;);
+
+
+
+
+	DSStartList(parts[6],PartSize);
+
+	DSListBegin(DS_QUADS);
+	for(int i=0;i<3;i++)
+	{
+		int sin_a1=isin(F(i)/3+1024);
+		int cos_a1=icos(F(i)/3+1024);
+		int sin_a2=isin(F(i+1)/3+1024);
+		int cos_a2=icos(F(i+1)/3+1024);
+		int sin_a3=isin((F(i)+F(0.5))/3+1024);
+		int cos_a3=icos((F(i)+F(0.5))/3+1024);
+		DSListNormal3n10(DSf32ton10(-cos_a3),DSf32ton10(-sin_a3),0);
+		DSListVertex3v16(cos_a1,sin_a1,F(0));
+		DSListVertex3v16(cos_a1,sin_a1,F(-1));
+		DSListVertex3v16(cos_a2,sin_a2,F(-1));
+		DSListVertex3v16(cos_a2,sin_a2,F(0));
+	}
+	DSListEnd();
+
+	DSListBegin(DS_QUAD_STRIP);
+	DSListNormal3f(0,0,1);
+	for(int i=0;i<=3;i++)
+	{
+		int sin_a=isin(F(i)/3+1024);
+		int cos_a=icos(F(i)/3+1024);
+		DSListVertex3v16(cos_a,sin_a,F(0));
+		DSListVertex3v16(imul(cos_a,F(3.9)),imul(sin_a,F(3.9)),F(0));
+	}
+	DSListEnd();
+
+	if(DSFinishList()>PartSize) for(;;);
 }
 
 void InitTunnel()
@@ -168,6 +298,10 @@ void InitTunnel()
 	MakeParts();
 
 	lastendsection=-1;
+	currhue=0;
+	x=y=0;
+	sx=sy=0;
+	sa=0;
 }
 
 void RunTunnel(int t)
@@ -212,25 +346,44 @@ void RunTunnel(int t)
 
 	for(int i=lastendsection+1;i<=endsection;i++)
 	{
-		tunnel[i%NumSections].part=Random()%NumParts;
-		tunnel[i%NumSections].x=isin(F(i)/23);
-		tunnel[i%NumSections].y=0;
+		sa+=(isin(t*8))/20;
+		sdx=imul(icos(sa),F(0.2));
+		sdy=imul(isin(sa),F(0.2));
+		sx+=sdx;
+		sy+=sdy;
+
+		if(Random()%10!=0)
+		{
+			int part=Random()%NumParts;
+			tunnel[i%NumSections].part=part;
+			tunnel[i%NumSections].hue=currhue;
+			tunnel[i%NumSections].x=sx;
+			tunnel[i%NumSections].y=sy;
+			currhue=(currhue+30)%360;
+		}
+		else
+		{
+			tunnel[i%NumSections].part=NumParts;
+			tunnel[i%NumSections].hue=0;
+			tunnel[i%NumSections].x=sx;
+			tunnel[i%NumSections].y=sy;
+		}
 	}
 	lastendsection=endsection;
 
 	for(int i=0;i<NumSections;i++)
 	{
-		int hue=(startsection+i%12)*30;
-		DSMaterialDiffuseAndAmbient(MakeHSV(hue,F(1),F(0.4)),MakeHSV(hue,F(1),F(0.7)));
-
 		int section=(startsection+i)%NumSections;
+
+		int hue=tunnel[section].hue;
+		DSMaterialDiffuseAndAmbient(MakeHSV(hue,F(1),F(0.4)),MakeHSV(hue,F(1),F(0.7)));
 
 		DSLoadIdentity();
 		DSTranslatef32(tunnel[section].x-x,tunnel[section].y-y,-F(i)+fraction);
 		//DSScalef(0.5,0.5,0.5);
 
 //		int type=Hash32(i)%;
-		if(tunnel[section].part!=NumParts-1)
+		if(tunnel[section].part<NumParts)
 		DSCallList(parts[tunnel[section].part]);
 	}
 
